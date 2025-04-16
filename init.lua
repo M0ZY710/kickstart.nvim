943ae8aca8c7a59e7e48169815f1efc17b692500
Jonzn-Jones <jonzn@d-ploid.com>
2025-04-16 18:25:46 +0200

    Added oil, clang-format config for ~/ and context

--- init.lua
+++ init.lua
@@ -727,6 +727,8 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        cpp = { 'clang_format' },
+        c = { 'clang_format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
